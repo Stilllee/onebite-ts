7354ff55d586e3f554d10d2bf13067738e0d3bc5
Stilllee <orodae@gmail.com>
2023-12-06 23:09:15 +0900

    [타입스크립트 컴파일러 옵션 설정하기] strict

--- src/hello.ts
+++ src/hello.ts
@@ -1,3 +1,7 @@
-export const hello = () => {
-  console.log("hello");
+/* 
+'message' 매개 변수에는 암시적으로 'any' 형식이 포함됩니다.
+라는 에러메세지 발생 
+*/
+export const hello = (message) => {
+  console.log("hello" + message);
 };
