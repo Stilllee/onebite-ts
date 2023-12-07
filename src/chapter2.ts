670e53cf187ef867bceb40154918b1b9dc278ad7
Stilllee <orodae@gmail.com>
2023-12-07 11:58:29 +0900

    [배열과 튜플] 유니언 타입

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -4,3 +4,6 @@ let numArr: number[] = [1, 2, 3];
 let strArr: string[] = ["Hello", "I'm", "Woodstock"];
 
 let boolArr: Array<boolean> = [true, false, true];
+
+// 배열에 들어가는 요소들의 타입이 다양할 경우
+let multiArr: (number | string)[] = [1, "hello"];
