abbfca020b5b2762a3b10b7f432ea0b586e0ac4a
Stilllee <orodae@gmail.com>
2023-12-07 16:52:02 +0900

    [객체] 선택적 프로퍼티(Optional Property)

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -1,20 +1,12 @@
 // object
 let user: {
-  id: number;
+  id?: number;
   name: string;
 } = {
   id: 1,
   name: "woodstock",
 };
 
-user.id;
-
-let cake: {
-  name: string;
-  brand: string;
-  cost: number;
-} = {
-  name: "스초생",
-  brand: "투썸",
-  cost: 3,
+user = {
+  name: "snoopy",
 };
