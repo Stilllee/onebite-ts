08ac42d3c1440fb6438acb207a0589bf209ce928
Stilllee <orodae@gmail.com>
2023-12-07 15:53:35 +0900

    [배열과 튜플] 다차원 배열

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -7,3 +7,9 @@ let boolArr: Array<boolean> = [true, false, true];
 
 // 배열에 들어가는 요소들의 타입이 다양할 경우
 let multiArr: (number | string)[] = [1, "hello"];
+
+// 다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+  [1, 2, 3],
+  [4, 5],
+];
