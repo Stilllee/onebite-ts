7c3658881e7bbbe25ce7dbfc66dd5e2dd2765b07
Stilllee <orodae@gmail.com>
2023-12-10 01:40:23 +0900

    [타입계층도와 기본타입] never

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -12,9 +12,30 @@ function unknownExam() {
   let e: unknown = undefined;
 
   // 다운캐스트는 불가능
-  let unknownVar: unknown;
+  /* let unknownVar: unknown;
 
   let num: number = unknownVar; // 오류메세지: 'unknown' 형식은 'number' 형식에 할당할 수 없습니다.
   let str: string = unknownVar; // 오류메세지: 'unknown' 형식은 'string' 형식에 할당할 수 없습니다.
-  let bool: boolean = unknownVar; // 오류메세지: 'unknown' 형식은 'boolean' 형식에 할당할 수 없습니다.
+  let bool: boolean = unknownVar; // 오류메세지: 'unknown' 형식은 'boolean' 형식에 할당할 수 없습니다. */
+}
+
+/**
+ * never 타입
+ * 타입계층도의 최하단
+ */
+
+function neverExam() {
+  function neverFunc(): never {
+    while (true) {}
+  }
+
+  // never타입은 모든 타입으로 업캐스트 가능
+  let num: number = neverFunc();
+  let str: string = neverFunc();
+  let bool: boolean = neverFunc();
+
+  // 다른 타입이 never타입으로 다운캐스트 불가능
+  let never1: never = 10; // 오류메세지: 'number' 형식은 'never' 형식에 할당할 수 없습니다.
+  let never2: never = "string"; // 오류메세지: 'string' 형식은 'never' 형식에 할당할 수 없습니다.
+  let never3: never = true; // 오류메세지: 'true' 형식은 'never' 형식에 할당할 수 없습니다.
 }
