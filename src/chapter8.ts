e37ce904833bdec7b3c2791649bb1d0b8e0f82fe
Stilllee <orodae@gmail.com>
2024-01-26 15:18:45 +0900

    [타입 좁히기] in 타입가드

--- src/chapter8.ts
+++ src/chapter8.ts
@@ -3,15 +3,14 @@
  * 조건문 등을 이용해 넓은 타입에서 좁은 타입으로
  * 타입을 상황에 따라 좁히는 방법
  */
-
 type Person = {
   name: string;
   age: number;
 };
-
 // value => number : toFixed
 // value => string : toUpperCase
 // value => Date : getTime
+// value => Person : name은(는) age살 입니다.
 function func(value: number | string | Date | null | Person) {
   if (typeof value === "number") {
     console.log(value.toFixed());
@@ -21,7 +20,7 @@ function func(value: number | string | Date | null | Person) {
   // instanceof 타입가드 : 내장 클래스 또는 직접 만든 클래스에만 사용가능, 직접 만든 타입과 함께 사용은 ❌
   else if (value instanceof Date) {
     console.log(value.getTime());
-  } else if (value instanceof Person) {
-    // Error : 'Person'은(는) 형식만 참조하지만, 여기서는 값으로 사용되고 있습니다.
+  } else if (value && "age" in value) {
+    console.log(`${value.name}은(는) ${value.age}살 입니다.`);
   }
 }
