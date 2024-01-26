aa734655edf5977f588eed1c67709a8caf7c6478
Stilllee <orodae@gmail.com>
2024-01-26 15:13:13 +0900

    [타입 좁히기] instanceof 타입가드

--- src/chapter8.ts
+++ src/chapter8.ts
@@ -4,16 +4,24 @@
  * 타입을 상황에 따라 좁히는 방법
  */
 
+type Person = {
+  name: string;
+  age: number;
+};
+
 // value => number : toFixed
 // value => string : toUpperCase
-function func(value: number | string) {
-  value; // value: number | string
-  value.toFixed(); // 'string | number' 형식에 'toFixed' 속성이 없습니다. 'string' 형식에 'toFixed' 속성이 없습니다.
-  value.toUpperCase(); // 'string | number' 형식에 'toUpperCase' 속성이 없습니다.   'number' 형식에 'toUpperCase' 속성이 없습니다.
-
+// value => Date : getTime
+function func(value: number | string | Date | null | Person) {
   if (typeof value === "number") {
     console.log(value.toFixed());
   } else if (typeof value === "string") {
     console.log(value.toUpperCase());
+  }
+  // instanceof 타입가드 : 내장 클래스 또는 직접 만든 클래스에만 사용가능, 직접 만든 타입과 함께 사용은 ❌
+  else if (value instanceof Date) {
+    console.log(value.getTime());
+  } else if (value instanceof Person) {
+    // Error : 'Person'은(는) 형식만 참조하지만, 여기서는 값으로 사용되고 있습니다.
   }
 }
