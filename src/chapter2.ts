fda8ca81d603803b74e0049eb5a3a757bbec9125
Stilllee <orodae@gmail.com>
2024-01-28 00:42:41 +0900

    [함수 타입 표현식과 호출 시그니쳐] 호출 시그니쳐와 하이브리드 타입

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -8,3 +8,21 @@ const add: (a: number, b: number) => number = (a, b) => a + b;
 const sub: Operation = (a, b) => a - b;
 const multiply: Operation = (a, b) => a * b;
 const divide: Operation = (a, b) => a / b;
+
+/**
+ * 호출 시그니쳐
+ * (콜 시그니쳐)
+ */
+
+type Operation2 = {
+  (a: number, b: number): number;
+  name: string; // 호출 시그니쳐 아래에 프로퍼티를 추가하면 함수이자 일반 객체를 의미하는 하이브리드 타입이 된다.
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
+
+add2(1, 2);
+add2.name;
