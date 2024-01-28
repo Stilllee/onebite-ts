86821a4092c25f1a6f9009baf0224d508f521a72
Stilllee <orodae@gmail.com>
2024-01-28 23:20:11 +0900

    [인터페이스 확장] 프로퍼티 재 정의하기

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -8,8 +8,15 @@ interface Animal {
 }
 
 interface Dog extends Animal {
+  name: "wawa"; // 프로퍼티 재정의
   isBark: boolean;
 }
+
+const dog: Dog = {
+  name: "와와", // Error : '"와와"' 형식은 '"wawa"' 형식에 할당할 수 없습니다.
+  color: "white",
+  isBark: true,
+};
 
 interface Cat extends Animal {
   isScratch: boolean;
@@ -18,9 +25,3 @@ interface Cat extends Animal {
 interface Chicken extends Animal {
   isFly: boolean;
 }
-
-const dog: Dog = {
-  name: "와와",
-  color: "white",
-  isBark: true,
-};
