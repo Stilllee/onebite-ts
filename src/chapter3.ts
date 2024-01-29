1c7e3bb130dae555a61ec028178389afbed3e260
Stilllee <orodae@gmail.com>
2024-01-29 20:45:59 +0900

    [접근 제어자] 필드 생략하기

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -5,12 +5,11 @@
  */
 
 class Employee {
-  // 필드
-  private name: string;
-  protected hp: number;
-  public position: string;
-
-  constructor(name: string, hp: number, position: string) {
+  constructor(
+    private name: string,
+    protected hp: number,
+    public position: string
+  ) {
     this.name = name;
     this.hp = hp;
     this.position = position;
@@ -40,7 +39,7 @@ class ExecutiveOfficer extends Employee {
   // 메서드
   func() {
     // private는 파생클래스에서도 접근이 불가능하다.
-    this.name; // Error : 'name' 속성은 private이며 'Employee' 클래스 내에서만 액세스할 수 있습니다.
+    // this.name; // Error : 'name' 속성은 private이며 'Employee' 클래스 내에서만 액세스할 수 있습니다.
 
     // protected는 파생클래스에서 접근이 가능하다.
     this.hp;
@@ -48,5 +47,8 @@ class ExecutiveOfficer extends Employee {
 }
 
 const employee = new Employee("woodstock", 90, "developer");
-employee.name = "Snoopy"; // Error : 'name' 속성은 private이며 'Employee' 클래스 내에서만 액세스할 수 있습니다.
-employee.hp = 79; // 'hp' 속성은 보호된 속성이며 'Employee' 클래스 및 해당 하위 클래스 내에서만 액세스할 수 있습니다.
+// employee.name = "Snoopy"; // Error : 'name' 속성은 private이며 'Employee' 클래스 내에서만 액세스할 수 있습니다.
+// employee.hp = 79; // 'hp' 속성은 보호된 속성이며 'Employee' 클래스 및 해당 하위 클래스 내에서만 액세스할 수 있습니다.
+
+employee.position = "디자이너";
+console.log(employee);
