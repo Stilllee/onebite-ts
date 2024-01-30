1f1c6be0c83ac38944c91cc0e17fefd11635aade
Stilllee <orodae@gmail.com>
2024-01-30 21:53:46 +0900

    [제네릭] 타입 변수 응용하기
    - 사례 2

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -6,3 +6,15 @@ function swap<T, U>(a: T, b: U) {
 }
 
 const [a, b] = swap("1", 2);
+
+/**
+ * 두번째 사례
+ */
+
+function returnFirstValue<T>(data: T[]) {
+  return data[0];
+}
+
+let num = returnFirstValue([0, 1, 2]);
+
+let str = returnFirstValue([1, "hello", "mynameis"]);
