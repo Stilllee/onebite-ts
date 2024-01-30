b83f7593ba5937463fe05890f3f11337f8d4123c
Stilllee <orodae@gmail.com>
2024-01-30 22:03:42 +0900

    [제네릭] 타입 변수 응용하기
    - 사례 3

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -11,7 +11,8 @@ const [a, b] = swap("1", 2);
  * 두번째 사례
  */
 
-function returnFirstValue<T>(data: T[]) {
+//  반환값의 타입을 배열의 첫번째 요소의 타입이 되도록 하려면 다음과 같이 튜플 타입과 나머지 파라미터를 이용하면 된다.
+function returnFirstValue<T>(data: [T, ...unknown[]]) {
   return data[0];
 }
 
