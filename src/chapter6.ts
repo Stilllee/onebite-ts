f923a8e22332cd8753d1515fc7e3bf4b088a8bad
Stilllee <orodae@gmail.com>
2024-02-02 22:27:57 +0900

    [프로미스와 제네릭] Promise 사용하기

---
+++ src/chapter6.ts
@@ -0,0 +1,20 @@
+/**
+ * 프로미스
+ */
+
+const promise = new Promise<number>((resolve, reject) => {
+  setTimeout(() => {
+    // resolve(20);
+    reject("~ 때문에 실패");
+  }, 3000);
+});
+
+promise.then((response) => {
+  console.log(response * 10);
+});
+
+promise.catch((err) => {
+  if (typeof err === "string") {
+    console.log(err);
+  }
+});
