25cea9028b333a782ea3424fdef5ecef50b7553f
Stilllee <orodae@gmail.com>
2024-02-06 00:22:00 +0900

    [맵드 타입 기반의 유틸리티 타입] Required

--- src/chapter1.ts
+++ src/chapter1.ts
@@ -19,3 +19,20 @@ const draft: Partial<Post> = {
   title: "제목 나중에 짓자",
   content: "초안...",
 };
+
+/**
+ * Required<T>
+ * -> 필수의, 필수적인
+ * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+type Required<T> = {
+  [key in keyof T]-?: T[key];
+};
+
+const withThumbnailPost: Required<Post> = {
+  title: "유틸리티 타입",
+  tags: ["ts"],
+  content: "",
+  thumbnailURL: "https://...",
+};
