1234c5eeeec67297789da9dbc307cce29f4f7921
Stilllee <orodae@gmail.com>
2024-02-05 23:04:48 +0900

    [infer] infer 예제 2

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -18,3 +18,16 @@ type B = ReturnType<FuncB>;
 type C = ReturnType<number>;
 // 조건식을 만족하는 R추론 불가능
 // never
+
+/**
+ * 예제 2.
+ */
+type PromiseUnpack<T> = T extends Promise<infer R> ? R : never;
+// 1. T는 프로미스 타입이어야 한다.
+// 2. 프로미스 타입의 결과값 타입을 반환해야 한다.
+
+type PromiseA = PromiseUnpack<Promise<number>>;
+// number
+
+type PromiseB = PromiseUnpack<Promise<string>>;
+// string
