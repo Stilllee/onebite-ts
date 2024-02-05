02efdbf7963fc0ce899b0268372fca57ce5df1c1
Stilllee <orodae@gmail.com>
2024-02-05 22:00:47 +0900

    [분산적인 조건부 타입] 분산적인 조건부 타입

---
+++ src/chapter2.ts
@@ -0,0 +1,26 @@
+/**
+ * 분산적인 조건부 타입
+ */
+type StringNumberSwitch<T> = T extends number ? string : number;
+
+let a: StringNumberSwitch<number>;
+
+let b: StringNumberSwitch<string>;
+
+let c: StringNumberSwitch<number | string>;
+// StringNumberSwitch<number> |
+// StringNumberSwitch<string>
+
+let d: StringNumberSwitch<boolean | number | string>;
+// 1단계
+// StringNumberSwitch<boolean> |
+// StringNumberSwitch<number> |
+// StringNumberSwitch<string>
+
+// 2단계
+// number |
+// string |
+// number
+
+// 결과
+// number | string
