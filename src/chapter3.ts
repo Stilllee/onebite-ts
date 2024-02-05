e520ec8d10d039494cc342b67b4d9c9dea457071
Stilllee <orodae@gmail.com>
2024-02-05 23:00:47 +0900

    [infer] infer 예제 1

---
+++ src/chapter3.ts
@@ -0,0 +1,20 @@
+/**
+ * infer
+ * inference -> 추론하다
+ */
+
+type ReturnType<T> = T extends () => infer R ? R : never;
+
+type FuncA = () => string;
+
+type FuncB = () => number;
+
+type A = ReturnType<FuncA>;
+// string
+
+type B = ReturnType<FuncB>;
+// number
+
+type C = ReturnType<number>;
+// 조건식을 만족하는 R추론 불가능
+// never
