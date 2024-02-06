b8e97976ea603915a8b4aaabef65e54b294fbe3a
Stilllee <orodae@gmail.com>
2024-02-06 19:54:15 +0900

    [맵드 타입 기반의 유틸리티 타입] Pick

---
+++ src/chapter2.ts
@@ -0,0 +1,21 @@
+/**
+ * Pick<T, K>
+ * -> 뽑다, 고르다
+ * -> 객체 타입으로부터 특정 프로퍼티만 딱 골라내는 그런 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  [key in K]: T[key];
+};
+
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "옛날 글",
+  content: "옛날 컨텐츠",
+};
