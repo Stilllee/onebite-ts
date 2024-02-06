caebbe1cf095ec07a431efdb8d7c33412d2bf01b
Stilllee <orodae@gmail.com>
2024-02-06 21:00:51 +0900

    [조건부 타입 기반의 유틸리티 타입] Extract

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -18,3 +18,12 @@ type Exclude<T, U> = T extends U ? never : T;
 // string
 
 type A = Exclude<string | boolean, boolean>;
+
+/**
+ * Extract<T, U>
+ * -> T에서 U를 추출하는 타입
+ */
+
+type Extract<T, U> = T extends U ? T : never;
+
+type B = Extract<string | boolean, boolean>;
