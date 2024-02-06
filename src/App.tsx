84e30048afef4e611f0a7a2943d1e2f7896259fb
Stilllee <orodae@gmail.com>
2024-02-06 22:06:15 +0900

    [상태관리와 Props] 투두리스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>할 일 목록</h1>
+      <Editor onClickAdd={onClickAdd} />
+    </div>
+  );
 }
 
 export default App;
